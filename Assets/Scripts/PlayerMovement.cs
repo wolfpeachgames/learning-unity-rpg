795c48534f62192d7bc0d4ba16943d5d1578c91e
tyrelh <krankyjoe@gmail.com>
2020-06-13 16:23:41 -0700

    add player movement script and enable 4 directions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (change != Vector3.zero) {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter() {
+        // multiplying by Time.deltaTime makes this a very small amount each frame
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
