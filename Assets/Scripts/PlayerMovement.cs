d4c87703cc632118926822b5b755c8ba47561fb9
tyrelh <krankyjoe@gmail.com>
2020-06-14 10:24:45 -0700

    format code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float speed;
     private Rigidbody2D myRigidbody;
@@ -10,28 +11,39 @@ public class PlayerMovement : MonoBehaviour {
     private Animator animator;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
 
-        if (change != Vector3.zero) {
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
             MoveCharacter();
             animator.SetFloat("moveX", change.x);
             animator.SetFloat("moveY", change.y);
             animator.SetBool("moving", true);
-        } else {
+        }
+        else
+        {
             animator.SetBool("moving", false);
         }
     }
 
-    void MoveCharacter() {
+    void MoveCharacter()
+    {
         // multiplying by Time.deltaTime makes this a very small amount each frame
         myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
     }
