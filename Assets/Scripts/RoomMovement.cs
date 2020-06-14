badbd98791a6de31737c622689fc91a16c7a5aba
tyrelh <krankyjoe@gmail.com>
2020-06-14 12:52:07 -0700

    add room labels on transition

--- Assets/Scripts/RoomMovement.cs
+++ Assets/Scripts/RoomMovement.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMovement : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public float displayTime;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,7 @@ public class RoomMovement : MonoBehaviour
         
     }
 
+    // Move from one map to another
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -27,6 +34,19 @@ public class RoomMovement : MonoBehaviour
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(displayTime);
+        text.SetActive(false);
     }
 }
