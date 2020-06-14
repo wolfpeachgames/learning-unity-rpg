893db51a821f88212539c6031436a09512f6b39a
tyrelh <krankyjoe@gmail.com>
2020-06-14 10:24:37 -0700

    camera will follow player with slight lag

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // LateUpdate always comes last
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
