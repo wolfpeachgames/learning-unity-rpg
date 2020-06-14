0010c24f9f46d19b7306a799649bf5a5dbcfbb98
tyrelh <krankyjoe@gmail.com>
2020-06-14 12:24:27 -0700

    camera transfer between rooms

---
+++ Assets/Scripts/RoomMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMovement : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange;
+        }
+    }
+}
