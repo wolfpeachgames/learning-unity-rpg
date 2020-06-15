52fd2b4b442a1d611fd00447d9ad1cdce9d9a26f
tyrelh <krankyjoe@gmail.com>
2020-06-14 22:28:46 -0700

    add simple follow ai to enemy log

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+
+    void CheckDistance()
+    {
+        if (ShouldChase())
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private bool ShouldChase()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        return (distance <= chaseRadius && distance > attackRadius);
+    }
+}
