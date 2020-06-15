fc5b7ed3ee9a0968eb28a813604b29bfe6245df8
tyrelh <krankyjoe@gmail.com>
2020-06-14 21:27:07 -0700

    add a breakable pot that interacts with the weapon hit boxes on player

---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Smash()
+    {
+        anim.SetBool("smash", true);
+        StartCoroutine(BreakCo());
+    }
+
+    IEnumerator BreakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
