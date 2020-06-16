5b4348470515d688e6be1e6b14d020f1b51281fe
tyrelh <krankyjoe@gmail.com>
2020-06-15 20:31:07 -0700

    wire up animations for log enemy

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -9,12 +9,15 @@ public class Log : Enemy
     public float attackRadius;
     public Transform homePosition;
     private Rigidbody2D myRigidBody;
+    public Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         currentState = EnemyState.IDLE;
         myRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
@@ -31,10 +34,17 @@ public class Log : Enemy
         if (ShouldChase())
         {
             Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            ChangeAnim(temp - transform.position);
             myRigidBody.MovePosition(temp);
             ChangeState(EnemyState.WALK);
+            anim.SetBool("wakeUp", true);
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
         }
     }
+
 
     private bool ShouldChase()
     {
@@ -42,6 +52,41 @@ public class Log : Enemy
         bool notBusy = (currentState == EnemyState.IDLE || currentState == EnemyState.WALK) && (currentState != EnemyState.STAGGER);
         return (notBusy && distance <= chaseRadius && distance > attackRadius);
     }
+
+
+    private void ChangeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+    }
+
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
 
     private void ChangeState(EnemyState newState)
     {
