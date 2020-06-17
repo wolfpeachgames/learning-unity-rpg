8999ca8885c4d75711a40e739a2b4ac7c3a7d829
tyrelh <krankyjoe@gmail.com>
2020-06-17 07:42:26 -0700

    add black fade transition to scene transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,6 +8,19 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1); // after 1 second
+        }
+    }
 
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -15,7 +28,23 @@ public class SceneTransition : MonoBehaviour
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            //SceneManager.LoadScene(sceneToLoad); // sync scene load
+            StartCoroutine(FadeCo()); // async scene load
+        }
+    }
+
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
--- Assets/Scripts/ScriptableObjects/VectorValue.cs
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public Vector2 initialValue;
+    public Vector2 defaultValue;
+
+    public void OnAfterDeserialize()
+    {
+        initialValue = defaultValue;
+    }
+
+    public void OnBeforeSerialize() { }
 }
