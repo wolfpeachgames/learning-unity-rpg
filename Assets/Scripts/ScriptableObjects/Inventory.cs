d9b9f991a1b7d581e9e52bf3d8771a1b04161a03
tyrelh <krankyjoe@gmail.com>
2020-06-17 21:06:59 -0700

    base ability to receive an item

---
+++ Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Inventory : ScriptableObject
+{
+    public Item currentItem;
+    public List<Item> items = new List<Item>();
+    public int numberOfKeys;
+}
