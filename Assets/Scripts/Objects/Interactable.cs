4de2c83dc65a468431310ba7f05ca08f4c77c5d7
tyrelh <krankyjoe@gmail.com>
2020-06-17 20:15:54 -0700

    refactor interactable elements out of signs

---
+++ Assets/Scripts/Objects/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool playerInRange;
+    public Signal contextClueSignal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInRange = true;
+            contextClueSignal.Raise();
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            Debug.Log("got here");
+            playerInRange = false;
+            contextClueSignal.Raise();
+        }
+    }
+}
