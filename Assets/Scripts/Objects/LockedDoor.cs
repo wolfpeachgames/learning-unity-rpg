f214bde4488afd55a679dc19efd913276b87256a
tyrelh <krankyjoe@gmail.com>
2020-06-19 21:38:01 -0700

    create locked door in dungeon, setup most of the scene transition to the dungeon

---
+++ Assets/Scripts/Objects/LockedDoor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType
+{
+    KEY, ENEMY, BUTTON
+}
+
+
+public class LockedDoor : Interactable
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool open = false;
+    public Inventory playerInventory;
+    public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;
+
+
+    private void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerInRange && thisDoorType == DoorType.KEY)
+            {
+                // does the player have a key?
+                if (playerInventory.numberOfKeys > 0)
+                {
+                    // remove a player key
+                    playerInventory.numberOfKeys--;
+                    // if so, call open
+                    Open();
+                }
+            }
+        }
+    }
+
+
+    public void Open()
+    {
+        // turn off the door's sprite renderer
+        doorSprite.enabled = false;
+        // set open to true
+        open = true;
+        // turn off the door's box collider
+        physicsCollider.enabled = false;
+    }
+
+
+    public void Close()
+    {
+
+    }
+}
