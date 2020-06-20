83f1fd8bfb3d0b8e9a6c71312a50e5ef2b87dfba
tyrelh <krankyjoe@gmail.com>
2020-06-20 08:23:17 -0700

    add heart item to replenish health

---
+++ Assets/Scripts/Objects/Heart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : Powerup
+{
+    public FloatValue playerHealth;
+    public float amountToIncrease;
+    public FloatValue heartContainers;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerHealth.RuntimeValue += amountToIncrease;
+            if (playerHealth.RuntimeValue > heartContainers.RuntimeValue * 2f)
+            {
+                playerHealth.RuntimeValue = heartContainers.RuntimeValue * 2f;
+            }
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+        }
+    }
+}
