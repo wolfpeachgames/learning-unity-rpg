ad7e3f8c87e94edade8c3e649d2322b113d62e35
tyrelh <krankyjoe@gmail.com>
2020-06-22 16:12:26 -0700

    should be named RoomObserver

--- Assets/Scripts/Game/RoomObserver.cs
+++ Assets/Scripts/Game/RoomObserver.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+public class RoomObserver : MonoBehaviour
 {
     public Enemy[] enemies;
     public Pot[] breakables;
