d0dd94b50df6c7c5ea10ef234d8a9bfa497df06c
tyrelh <krankyjoe@gmail.com>
2020-06-25 20:34:21 -0700

    create pause menu

---
+++ Assets/Scripts/Game/PauseManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject pausePanel;
+    public string startMenu;
+
+
+    void Start()
+    {
+        isPaused = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("pause"))
+        {
+            TogglePause();
+        }
+    }
+
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+
+    public void QuitToMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(startMenu);
+    }
+}
