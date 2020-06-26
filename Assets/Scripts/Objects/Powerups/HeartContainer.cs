30d8b5bc6bdf9bbaf85384ca85cd4e61860f4138
tyrelh <krankyjoe@gmail.com>
2020-06-25 17:51:01 -0700

    reorganize object scripts

---
+++ Assets/Scripts/Objects/Powerups/HeartContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartContainer : Powerup
+{
+    public FloatValue heartContainers;
+    public FloatValue playerHealth;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // increase number of heart containers
+            heartContainers.RuntimeValue++;
+            // fill player health
+            playerHealth.RuntimeValue = heartContainers.RuntimeValue * 2;
+            // raise health signal
+            powerupSignal.Raise();
+            // destroy
+            Destroy(this.gameObject);
+        }
+    }
+}
