6e72f912691060d3c65955021e01173039ddbbaf
tyrelh <krankyjoe@gmail.com>
2020-06-25 19:09:30 -0700

    start screen with options to quit and start new game

---
+++ Assets/Scripts/Game/StartScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public string startingSceneForNewGame;
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startingSceneForNewGame);
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
