c2ac1217fd95ca6827c990d01894f690340ed6ee
tyrelh <krankyjoe@gmail.com>
2020-06-27 17:09:57 -0700

    create GameSaveManager that persists data between switching scenes

---
+++ Assets/Scripts/Game/GameSaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSaveManager : MonoBehaviour
+{
+    public static GameSaveManager gameSave;
+    public List<ScriptableObject> objects = new List<ScriptableObject>();
+
+
+    private void Awake()
+    {
+        if (gameSave == null)
+        {
+            gameSave = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
