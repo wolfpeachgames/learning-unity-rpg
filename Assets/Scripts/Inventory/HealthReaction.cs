a935810469b465bcc56b341fd2509821f49b1d2e
tyrelh <krankyjoe@gmail.com>
2020-06-29 14:37:40 -0700

    health and magic potions and their effects

---
+++ Assets/Scripts/Inventory/HealthReaction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthReaction : MonoBehaviour
+{
+    public FloatValue playerHealth;
+    public Signal healthSignal;
+
+
+    public void Use(int amountToIncrease)
+    {
+        playerHealth.RuntimeValue += amountToIncrease;
+        healthSignal.Raise();
+    }
+}
