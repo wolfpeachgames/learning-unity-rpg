bf6138239b79493a1044cd7e2e96c5b00100f1fb
tyrelh <krankyjoe@gmail.com>
2020-06-29 09:52:50 -0700

    inventory infrastructure WIP

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Inventory Info")]
+    public PlayerInventory playerInventory;
+    [SerializeField] private GameObject blankInventorySlot;
+    [SerializeField] private GameObject inventoryContentsPanel;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private GameObject useButton;
+
+
+    public void SetTextAndButton(string description, bool buttonActive)
+    {
+        descriptionText.text = description;
+        useButton.SetActive(buttonActive);
+    }
+
+
+    private void MakeInventorySlots()
+    {
+        if (playerInventory)
+        {
+            for (int i = 0; i < playerInventory.items.Count; i++)
+            {
+                GameObject blankItemSlot = Instantiate(blankInventorySlot, inventoryContentsPanel.transform.position, Quaternion.identity);
+                blankItemSlot.transform.SetParent(inventoryContentsPanel.transform);
+                InventorySlot newSlot = blankItemSlot.GetComponent<InventorySlot>();
+                if (newSlot)
+                {
+                    newSlot.Setup(playerInventory.items[i], this);
+                }
+            }
+        }
+    }
+
+
+    void Start()
+    {
+        MakeInventorySlots();
+        SetTextAndButton("", false); // initialize as blank
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI itemNumberText;
+    [SerializeField] private Image itemImage;
+
+    [Header("Variables from the Item")]
+    public InventoryItem item;
+
+    [Header("Inventory Manager")]
+    public InventoryManager inventoryManager;
+
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager)
+    {
+        item = newItem;
+        inventoryManager = newManager;
+        if (item)
+        {
+            itemImage.sprite = item.itemImage;
+            itemNumberText.text = item.numberHeld.ToString();
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
