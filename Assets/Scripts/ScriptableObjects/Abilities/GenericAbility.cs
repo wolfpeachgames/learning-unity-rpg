d3371de655a429964c1659f102b8237078f7b157
tyrelh <krankyjoe@gmail.com>
2020-06-30 15:54:43 -0700

    basic architecture for ability system

---
+++ Assets/Scripts/ScriptableObjects/Abilities/GenericAbility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Abilities/GenericAbility", fileName = "NewGenericAbility")]
+public class GenericAbility : ScriptableObject
+{
+    public virtual void Ability(Vector2 playerPosition, Vector2 playerFacingDirection, Animator playerAnimator = null, Rigidbody2D playerRigidbody = null)
+    {
+
+    }
+}
